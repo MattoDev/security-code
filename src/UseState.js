ad1a27ea9b78b7efd33eb13bb8f60ffb293c118f
MattoDev <mateofonseca.profesional@gmail.com>
2022-04-06 07:51:19 -0500

    Include useEffect

--- src/UseState.js
+++ src/UseState.js
@@ -2,13 +2,27 @@ import React from "react";
 
 function UseState({ name }) {
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("Empezando el efecto");
+    if (!!loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validacion");
+        setLoading(false);
+        console.log("terminando la validacion");
+      }, 3000);
+    }
+    console.log("Terminando el efecto");
+  }, [loading]);
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el codigo de seguridad.</p>
       {error && <p>Error: el codigo es incorrecto</p>}
+      {loading && <p>Cargando...</p>}
       <input placeholder="Codigo de seguridad" />
-      <button onClick={() => setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 }
