d9498a5bb9f49b61c603f778f35f648e40e2f18f
MattoDev <mateofonseca.profesional@gmail.com>
2022-04-08 07:50:14 -0500

    use componentDidUpdate

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       error: false,
+      loading: false,
     };
+  }
+
+  // UNSAFE_componentWillMount() {
+  //   console.log("componentWillMount");
+  // }
+
+  // componentDidMount() {
+  //   console.log("componentDidMount");
+  // }
+
+  componentDidUpdate() {
+    console.log("Actualizacion");
+    if (!!this.state.loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validacion");
+        this.setState({ loading: false });
+        // setLoading(false);
+        console.log("terminando la validacion");
+      }, 3000);
+    }
   }
 
   render() {
@@ -14,8 +36,9 @@ class ClassState extends React.Component {
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el codigo de seguridad.</p>
         {this.state.error && <p>Error: el codigo es incorrecto</p>}
+        {this.state.loading && <Loading />}
         <input placeholder="Codigo de seguridad" />
-        <button onClick={() => this.setState({ error: !this.state.error })}>
+        <button onClick={() => this.setState({ loading: !this.state.loading })}>
           Comprobar
         </button>
       </div>
